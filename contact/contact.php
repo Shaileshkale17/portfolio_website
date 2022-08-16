33a37da203cf21a45dab6f8ff771d65d156c8ed7
Shailesh <shaileshkale87730@gmail.com>
2022-08-16 17:24:12 +0530

    git commit "remove / bar"

--- contact/contact.php
+++ contact/contact.php
@@ -23,7 +23,7 @@ $sql =SELECT * FROM `contactus`
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shailesh kale</title>
     <!-- <link rel="stylesheet" href="/style.css"> -->
-    <link rel="stylesheet" href="/contact/contact-style.css">
+    <link rel="stylesheet" href="contact/contact-style.css">
 </head>
 
 <body>
